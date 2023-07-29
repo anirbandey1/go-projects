8631a6766d358db7e7ff183e474cb472462ae306
Anirban Dey <116617392+anirbandey1@users.noreply.github.com>
2023-07-29 12:21:41 +0530

    Go-server form not showing up

--- go-server/main.go
+++ go-server/main.go
@@ -7,16 +7,49 @@ import (
 )
 
 
+func helloHandler(w http.ResponseWriter, r *http.Request)  {
+   if r.URL.Path != "/hello"{
+        http.Error(w,"404 not found", http.StatusNotFound)
+   }
+
+   if r.Method != "GET"{
+        http.Error(w,"Method is not supported",http.StatusNotFound)
+        return 
+   }
+
+   fmt.Fprintf(w,"Hello!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request)  {
+    if err:= r.ParseForm(); err != nil {
+        fmt.Fprintf(w,"ParseForm() err: %v",err)
+        return
+    }
+
+    fmt.Fprintf(w,"Post Request Successful\n")
+
+    name := r.FormValue("name")
+    address := r.FormValue("address")
+    fmt.Fprintf(w,"Name = %s\n",name)
+    fmt.Fprintf(w,"Address = %s\n",address)
+}
+
 func main(){
 
-    fileServer := http.FileServer(http.Dir('./static/'))
+    // fileServer := http.FileServer(http.Dir('./static/'))
+    fileServer := http.FileServer(http.Dir("./static"))
     http.Handle("/",fileServer)
+
+    // http.Handle("/static/", http.StripPrefix("/static/", fileServer))
     http.HandleFunc("/form",formHandler)
     http.HandleFunc("/hello",helloHandler)
 
     fmt.Printf("Server starting at 8080 port")
 
-    if err:
+
+    if err:= http.ListenAndServe(":8080",nil); err!=nil {
+        log.Fatal(err)
+    }
 
 }
 
